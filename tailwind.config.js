fc28595c0e73dd4838c71ff3db94c55809028f57
Sergej Smelov <smelowsergey@gmail.com>
2023-11-13 21:52:03 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {},
+    colors: {
+      "dark-cyan": "hsl(158, 36%, 37%)",
+      "cream": "hsl(30, 38%, 92%)",
+      "very-dark-blue": "hsl(212, 21%, 14%)",
+      "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      white: "hsl(0, 0%, 100%)"
+    },
+    fontFamily: {
+      "main": ["Montserrat", "sans-serif"],
+      "heading": ["Fraunces", "serif"]
+    },
+    fontSize: {
+      paragraph: "14px"
+    },
+    borderRadius: {
+      "10": "10px",
+    }
+  },
+  plugins: [],
+}
+
