9289a53fa95f6a5b80d2c29eaafca55beab7bbe6
Sergej Smelov <smelowsergey@gmail.com>
2023-11-14 20:36:04 +0500

    Write the whole page at once

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
     extend: {},
+    screens: {
+      md: "40rem",
+      lg: "64rem"
+    },
     colors: {
       "dark-cyan": "hsl(158, 36%, 37%)",
       "cream": "hsl(30, 38%, 92%)",
       "very-dark-blue": "hsl(212, 21%, 14%)",
       "dark-grayish-blue": "hsl(228, 12%, 48%)",
+      black: "hsl(0, 0%, 0%)",
       white: "hsl(0, 0%, 100%)"
     },
     fontFamily: {
@@ -16,9 +21,14 @@ export default {
       "heading": ["Fraunces", "serif"]
     },
     fontSize: {
-      paragraph: "14px"
+      xs: "11px",
+      s: "12px",
+      base: "14px",
+      l: "2rem"
+
     },
     borderRadius: {
+      "0": "0px",
       "10": "10px",
     }
   },
